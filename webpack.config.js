ae5606bf3cf9c4a02a4d79adb3bb78624102e7f3
briptimus <brituor@gmail.com>
2016-01-15 19:40:44 -0800

    Configuring babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,14 @@ const PATHS = {
 
 
 const common = {
-  // Entry accepts a path or an object of entries.
-  // The build chapter contains an example of the latter.
   entry: PATHS.app,
+  
+  // Add resolve.extensions. '' is needed to allow imports
+  // without an extension. Note the .'s before extensions!!!
+  // The matching will fail without!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },  
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
@@ -22,12 +27,20 @@ const common = {
   module: {
     loaders: [
       {
-        // Test expects a RegExp! Note the slashes!
         test: /\.css$/,
         loaders: ['style', 'css'],
-        // Include accepts either a path or an array of paths.
         include: PATHS.app
-      }
+      },
+
+      // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }      
     ]
   },  
   plugins: [
